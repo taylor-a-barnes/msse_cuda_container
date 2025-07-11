8f2cb66ed0646e9c617c2a549d1dae9d92e0e9cf
Taylor Barnes <taylor.a.barnes@gmail.com>
2025-07-11 16:57:49 -0400

    Improved example

--- testscripts/05_pes.py
+++ testscripts/05_pes.py
@@ -39,16 +39,15 @@ class Layer:
             #self.activations = np.maximum(0, self.rawactivations) # ReLU
 
     def backpropagation(self, reference=None):
-
-        if reference is None:
+        if self.next_layer is None: # Output layer
+            # Get this from differentiating the cost function
+            self.delta = 2.0 * (self.activations - reference)
+        else: # Hidden layer
             # Get this from differentiating the activation function
             activation_grad = self.activations * (1.0 - self.activations) # Sigmoid
             #activation_grad = np.where(self.rawactivations > 0, 1.0, 0.0) # ReLU
             self.delta = np.dot(self.next_layer.weights.transpose(), self.next_layer.delta) * activation_grad
-        else:
-            # Get this from differentiating the cost function
-            self.delta = 2.0 * (self.activations - reference)
-            
+
         self.biases_grad += self.delta
         for i, delt in enumerate(self.delta):
             for j, act in enumerate(self.previous_layer.activations):
@@ -59,6 +58,42 @@ class Layer:
         self.biases = self.biases - ( training_rate / batch_size ) * self.biases_grad
         self.weights_grad.fill(0.0)
         self.biases_grad.fill(0.0)
+
+class Network:
+    def __init__(self, sizes, training_inputs, training_references):
+        self.layers = []
+        self.training_inputs = training_inputs
+        self.training_references = training_references
+
+        new_layer = InputLayer(1)
+        self.layers.append( new_layer )        
+        for ilayer in range( 1, len(sizes) ):
+            self.layers.append( Layer(sizes[ilayer], self.layers[-1]) )
+            print(f"ilayer: {ilayer}")
+
+    def train(self, nepochs):
+        for iepoch in range(nepochs):
+            batch_size = ninputs
+            indices = np.random.permutation(batch_size)
+            inputs_shuffled = self.training_inputs[indices]
+            refs_shuffled = self.training_references[indices]
+            for iref in range(batch_size):
+                reference = refs_shuffled[iref]
+                
+                # Set the input layer
+                self.layers[0].activations[0] = inputs_shuffled[iref]
+
+                # Feedforward through the other layers
+                for ilayer in range( 1, len(self.layers) ):
+                    self.layers[ilayer].feedforward()
+                print(f"Output, ref: {self.layers[-1].activations}, {reference}")
+
+                # Do backpropagation
+                for ilayer in range( len(self.layers)-1 ):
+                    self.layers[-1-ilayer].backpropagation(reference)
+
+            for ilayer in range( 1, len(self.layers) ):
+                self.layers[ilayer].apply_gradient(batch_size, training_rate)
 
 
 def morse_potential(De, re, a, r):
@@ -76,7 +111,6 @@ training_rate = 0.1
 
 # Randomly generate a set of distances
 rvalues = np.float32( np.random.uniform(min_rvalue, max_rvalue, (ninputs,)) )
-#print(f"rvalues: {rvalues}")
 
 # Generate the reference energies for each of these distances
 erefs = np.empty( (ninputs,), dtype=np.float32 )
@@ -84,35 +118,8 @@ for idx, r in enumerate(rvalues):
     erefs[idx] = morse_potential(De, re, a, r)
 print(f"erefs: {erefs}")
 
-# Create the input layer
-input_layer = InputLayer(1)
 
-# Create the hidden layer
-hidden_layer = Layer(16, input_layer)
-hidden_layer2 = Layer(16, hidden_layer)
+net = Network( [1, 16, 16, 1], rvalues, erefs )
+net.train( 200 )
 
-# Create the output layer
-output_layer = Layer(1, hidden_layer2)
-
-for iepoch in range(1000):
-    batch_size = ninputs
-    indices = np.random.permutation(batch_size)
-    rvalues_shuffled = rvalues[indices]
-    erefs_shuffled = erefs[indices]
-    for iref in range(batch_size):
-        reference = erefs_shuffled[iref]
-        input_layer.activations[0] = ( rvalues_shuffled[iref] - min_rvalue ) / ( max_rvalue - min_rvalue )
-        hidden_layer.feedforward()
-        hidden_layer2.feedforward()
-        output_layer.feedforward()
-        print(f"Output, ref: {output_layer.activations}, {reference}")
-
-        # Do backpropagation
-        output_layer.backpropagation(reference)
-        hidden_layer2.backpropagation()
-        hidden_layer.backpropagation()
-
-    output_layer.apply_gradient(batch_size, training_rate)
-    hidden_layer2.apply_gradient(batch_size, training_rate)
-    hidden_layer.apply_gradient(batch_size, training_rate)
 
